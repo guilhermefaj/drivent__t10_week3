589359dbe8c4d6b69f1db21dcc026af00e20571d
guilhermefaj <figueiredoguiu@gmail.com>
2023-07-13 17:10:17 -0300

    Fix: cleanDB

--- tests/helpers.ts
+++ tests/helpers.ts
@@ -6,13 +6,14 @@ import { createSession } from './factories/sessions-factory';
 import { prisma } from '@/config';
 
 export async function cleanDb() {
-  await prisma.ticket.deleteMany({});
-  await prisma.ticketType.deleteMany({});
-  await prisma.payment.deleteMany({});
   await prisma.address.deleteMany({});
+  await prisma.payment.deleteMany({});
+  await prisma.ticket.deleteMany({});
   await prisma.enrollment.deleteMany({});
+  await prisma.event.deleteMany({});
   await prisma.session.deleteMany({});
   await prisma.user.deleteMany({});
+  await prisma.ticketType.deleteMany({});
   await prisma.booking.deleteMany({});
   await prisma.room.deleteMany({});
   await prisma.hotel.deleteMany({});
